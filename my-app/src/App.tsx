c2fa7e6e16fcc031a4aec969f631485fd8744214
Hamza Tamim <hamza.tamim@asg-passau.de>
2022-05-21 11:58:26 +0200

    Grid

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
 
 import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
@@ -14,6 +12,33 @@ import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
+
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Avatar } from '@mui/material';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+function UserGrid() {
+  var benutzer = Benutzer();
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {benutzer.map((_, index) => (
+          <Grid item xs={4} sm={4} md={4} key={index}>
+            <Item>{benutzer[index]}</Item>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+}
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -94,15 +119,17 @@ function SearchAppBar() {
   );
 }
 
+function Benutzer(){
+  var benutzerArray = [<><Avatar>a</Avatar>name</>,<Avatar>b</Avatar>,<Avatar>c</Avatar>,<Avatar>d</Avatar>,<Avatar>e</Avatar>,<Avatar>f</Avatar>]
+  return(benutzerArray);
+}
 
 function App() {
   return ( 
     <>
     <SearchAppBar/>
-    <Stack spacing={2} direction="row">
-      <Avatar>H</Avatar>
-      <Button variant="contained">Contained</Button>
-    </Stack>
+    <UserGrid/>
+    <Button variant="contained">Contained</Button>
     </>
   );
 }
